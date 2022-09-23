8d786c04271032d997f6ae0497f3cbd8abd075fa
uycoder <Thunder1987>
2022-09-24 02:30:36 +0300

    learn bubble sort

---
+++ src/main/java/dev/ahmed/BubbelSort.java
@@ -0,0 +1,34 @@
+package dev.ahmed;
+
+
+import java.util.Arrays;
+
+/**
+ * @author Ahmed Bughra
+ * @create 2022-09-24  2:12 AM
+ */
+public class BubbelSort {
+    public static void main(String[] args) {
+        int[] a = {95, 68, 18, 94, 4, 83, 47, 8,44};
+        bubble(a);
+    }
+
+    private static void bubble(int[] a) {
+        for (int j = 0; j < a.length - 1; j++) {
+
+            for (int i = 0; i < a.length - 1; i++) {
+                if (a[i] > a[i + 1]) {
+                    swap(a, i, i + 1);
+                }
+            }
+            System.out.println("No. " + j + " loop for bubbleSort " + Arrays.toString(a));
+        }
+    }
+
+    private static void swap(int[] a, int i, int j) {
+        int t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+}
